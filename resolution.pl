66e7f0a5b037a7260dbdbad6763301ffee12ceba
Armand Nicolicioiu <armand.nicolicioiu@gmail.com>
2020-12-01 22:05:02 +0200

    Implemented negations

---
+++ resolution.pl
@@ -0,0 +1,20 @@
+negation(X, NotX):-
+    term_to_atom(X, XAtom),
+    atom_chars(XAtom, XChars),
+    not(is_negated(XChars)),
+    NotX = n(X).
+
+negation(X, NotX):-
+    term_to_atom(X, XAtom),
+    atom_chars(XAtom, XChars),
+    is_negated(XChars),
+    atom_length(XAtom, Length),
+    DesiredLeength is Length - 3,
+    sub_atom(XAtom, 2, DesiredLeength, _, NotXAtom),
+    term_to_atom(NotX, NotXAtom).
+
+is_negated([FirstChar, SecondChar | RestChars]):-
+    FirstChar == 'n',
+    SecondChar == '(',
+    last(RestChars, LastChar),
+    LastChar == ')'.
