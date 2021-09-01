8a78d8b3fa90e4b43880b853070a67768dc42019
Armand Nicolicioiu <armand.nicolicioiu@gmail.com>
2021-09-01 17:30:01 +0300

    Add degree curves placeholder and values extractor

--- vagueness.pl
+++ vagueness.pl
@@ -6,7 +6,29 @@ main:-
     [FirstRule | _] = Rules,
     [Connector | [[Premise1, Premise2], [Conclusion]]] = FirstRule,
 
+    Curves = [[service, poor, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]],
+              [service, good, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]],
+              [service, excellent, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]],
+              
+              [food, rancid, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]],
+              [food, delicious, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]],
+              
+              [tip, stingy, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]],
+              [tip, normal, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 50, 13]],
+              [tip, generous, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]]],
+
+    get_value(tip, normal, Curves, 11, YValue),
+    write('Value: '), write(YValue), nl, nl,
+
     write(Connector), nl,
     write(Premise1), nl,
     write(Premise2), nl,
     write(Conclusion), nl.
+
+
+get_value(Attribute, Category, Curves, XValue, YValue):-
+    select(AttributeCurve, Curves, _),
+    [Attribute, Category, CurveValues] = AttributeCurve,
+    nth0(XValue, CurveValues, YValue).
+
+% solve(ServiceRating, FoodRating, Curves, TipPercentage):-
